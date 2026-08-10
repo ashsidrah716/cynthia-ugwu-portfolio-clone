4dc2d7b20cc9fa07caf95ab8660be20dcb1fe525
ashsidrah716 <ashsidrah7@gmail.com>
2026-08-10 19:12:26 +0500

    Implement interactive image hover effects

--- script.js
+++ script.js
@@ -43,5 +43,54 @@ function mouseMovementSqueeze() {
   });
 }
 
+// image hover effect
+document.querySelectorAll(".element").forEach(function (element) {
+  let img = element.querySelector("img");
+  let prevPosition = 0;
+  let rotateDifference = 0;
+  let rotation = 0;
+  let timer2;
+
+  // image disappearing after mouse leaves
+  element.addEventListener("mouseleave", function (e) {
+    clearTimeout(timer2);
+
+    gsap.to(img, {
+      rotate: 0,
+      opacity: 0,
+      ease: "power1.out",
+    });
+  });
+
+  // image movement
+  element.addEventListener("mousemove", function (details) {
+    clearTimeout(timer2);
+
+    let difference = details.clientY - element.getBoundingClientRect().top;
+    rotateDifference = details.clientX - prevPosition;
+    prevPosition = details.clientX;
+
+    rotation = gsap.utils.clamp(
+      -50,
+      50,
+      gsap.utils.mapRange(-80, 80, -50, 50, rotateDifference),
+    );
+
+    gsap.to(img, {
+      opacity: 1,
+      ease: "power1.out",
+      top: difference - img.offsetHeight / 2,
+      left: details.clientX - img.offsetWidth / 2,
+      rotate: rotation,
+    });
+
+    timer2 = setTimeout(function () {
+      gsap.to(img, {
+        rotate: 0,
+      });
+    }, 100);
+  });
+});
+
 // Function calls
 mouseMovementSqueeze();
